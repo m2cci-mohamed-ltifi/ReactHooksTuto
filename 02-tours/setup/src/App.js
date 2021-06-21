2b4a3706590c14c5ff1b73959f3fa5fdac1df305
m2cci-mohamed-ltifi <m2cci-mohamed-ltifi@users.noreply.github.com>
2021-06-21 14:11:58 +0200

    removing items

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -7,6 +7,12 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -29,9 +35,19 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div>
+          <h4>no tours left</h4>
+          <button onClick={() => fetchTours()}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
